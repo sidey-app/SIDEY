35ba38320df88c945ed58b87ac2d3d40a5008819
Ryu Tae Hyun <ryu200112@gmail.com>
2026-09-01 13:05:17 +0900

    fix(windows): compile monitor and owner controls

--- windows/src/Sidey.App/ComposerWindow.xaml.cs
+++ windows/src/Sidey.App/ComposerWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Sidey.Core.Domain;
+using Sidey.Platform.Windows;
 using Windows.System;
 
 namespace Sidey.App;
